d930689cc428165a385157a9c3dd9eb2706b6da6
Stepan Boychuk <demonomiconnnn@gmail.com>
2023-10-16 14:49:57 +0300

    Used promisify

--- verify-environment.js
+++ verify-environment.js
@@ -3,27 +3,21 @@ const { exec } = require('child_process');
 const nvmInitScript = '~/.nvm/nvm.sh';
 const toolsList = ['docker', 'git', 'npm', 'nvm', 'node'];
 
-const runCommand = (toolName) => {
+const toolVersionCheck = (tool) => {
   return new Promise((resolve) => {
-    exec(`/bin/bash -c "source ${nvmInitScript} && ${toolName} --version"`, (error, stdout, stderr) => {
-      resolve(stdout);
-    });
+      exec(`/bin/bash -c "source ${nvmInitScript} && ${tool} --version"`, (error, stdout, stderr) => {
+        if (stdout){
+          resolve(console.log(`${tool}: ${stdout}`));
+        }else{
+          throw new Error(`${tool} not found.`);
+        }
+      });
   });
 }
 
-const envVersionCheck = async (toolName) => {
-  const toolVersion = await runCommand(toolName);
-  if(toolVersion) {
-    console.log(`${toolName}: ${toolVersion}`);
-  } else {
-    console.log(`${toolName} not found`);
-    process.exit(1);
-  }
-}
-
-toolsList.forEach(tool => {
-  envVersionCheck(tool).catch(error => {
-    console.error(error);
+Promise.all(toolsList.map(tool => {
+  toolVersionCheck(tool).catch(error => {
+    console.log(error)
     process.exit(1)
   });
-});
+}));
