e403a9949f8e22a3757c9d97475b4f0cb7a705a1
Stepan Boychuk <demonomiconnnn@gmail.com>
2023-10-17 14:27:30 +0300

    Code review feedback

--- verify-environment.js
+++ verify-environment.js
@@ -7,18 +7,16 @@ const toolsList = ['docker', 'git', 'npm', 'nvm', 'node'];
 const promisifiedExec = util.promisify(exec);
 
 const toolVersionCheck = async (toolName) => {
-  try {
-    const { stdout } = await promisifiedExec(`/bin/bash -c "source ${nvmInitScript} && ${toolName} --version"`)
-    return stdout
-  } catch (error) {
-    console.error(`${toolName} not found`);
-    process.exit(4);
-  }
+  const { stdout } = await promisifiedExec(`/bin/bash -c "source ${nvmInitScript} && ${toolName} --version"`);
+  return stdout;
 }
 
 Promise.all(toolsList.map(tool => {
   toolVersionCheck(tool)
     .then(result => {
       console.log(`${tool}: ${result}`);
-    });
+    }).catch(error => {
+      console.error(`${tool} not found`);
+      process.exit(error.code);
+    })
 }));
